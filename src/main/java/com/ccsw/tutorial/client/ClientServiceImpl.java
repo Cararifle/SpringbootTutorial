e4b2d7ea0ac34550ed80bdaccad3c9be443a0239
Ferran Grande Pla <fgrandepla@gmail.com>
2026-04-21 13:47:11 +0200

    feat: implementar operaciones del cliente

---
+++ src/main/java/com/ccsw/tutorial/client/ClientServiceImpl.java
@@ -0,0 +1,54 @@
+package com.ccsw.tutorial.client;
+
+import com.ccsw.tutorial.client.model.Client;
+import com.ccsw.tutorial.client.model.ClientDto;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class ClientServiceImpl implements ClientService {
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<Client> findAll() {
+
+        return (List<Client>) this.clientRepository.findAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void save(Long id, ClientDto dto) {
+
+        Client client;
+
+        if (id == null) {
+            client = new Client();
+        } else {
+            client = this.clientRepository.findById(id).orElse(null);
+        }
+
+        client.setName(dto.getName());
+
+        this.clientRepository.save(client);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void delete(Long id) throws Exception {
+
+        if (this.clientRepository.findById(id).orElse(null) == null) {
+            throw new Exception("Not exists");
+        }
+
+        this.clientRepository.deleteById(id);
+    }
+}
