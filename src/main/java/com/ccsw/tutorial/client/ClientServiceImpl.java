1083e9b3a431c42bb0304722a063cac032ca7390
Cararifle <fgrandepla@gmail.com>
2026-04-22 12:09:03 +0200

    fix: tratamiento de la excepcion por duplicacion del nombre del cliente

--- src/main/java/com/ccsw/tutorial/client/ClientServiceImpl.java
+++ src/main/java/com/ccsw/tutorial/client/ClientServiceImpl.java
@@ -2,6 +2,7 @@ package com.ccsw.tutorial.client;
 
 import com.ccsw.tutorial.client.model.Client;
 import com.ccsw.tutorial.client.model.ClientDto;
+import com.ccsw.tutorial.exception.DuplicateClientException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,7 +32,7 @@ public class ClientServiceImpl implements ClientService {
         Client client;
 
         if (clientRepository.existsByName(dto.getName())) {
-            throw new RuntimeException("Ya existe un cliente con ese nombre");
+            throw new DuplicateClientException("Ya existe un cliente con ese nombre");
         }
 
         if (id == null) {
